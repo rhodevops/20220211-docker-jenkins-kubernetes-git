638f6bd354277892533cf968dd53ae22b1b496bd
rhodevops <rhodevops@gmail.com>
2022-06-14 10:48:24 +0200

    Cambiar puertos y agregar imagen

--- projects/p205/billing/target/generated-sources/annotations/com/paymentchain/billing/common/InvoiceRequestMapperImpl.java
+++ projects/p205/billing/target/generated-sources/annotations/com/paymentchain/billing/common/InvoiceRequestMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-06-09T09:45:38+0200",
+    date = "2022-06-14T10:32:45+0200",
     comments = "version: 1.3.1.Final, compiler: Eclipse JDT (IDE) 1.4.100.v20220318-0906, environment: Java 17.0.3 (Eclipse Adoptium)"
 )
 @Component
